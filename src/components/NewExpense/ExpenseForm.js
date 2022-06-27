adce466b3a16e60f6293a13307f5e58ad9de366d
Piotr <piotr.franczak@cledar.pl>
2022-06-27 12:16:58 +0200

    second push

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -85,7 +85,7 @@ function ExpenseForm(props) {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="button" onClick={props.onCancel} type="submit">
+        <button type="button" onClick={props.onCancel}>
           Cancel
         </button>
         <button type="submit">Add Expense</button>
